0d936a16abd29e216ad2ff6bd07272f6ca2a64db
fowenati <fowenati.la@gmail.com>
2023-07-18 22:41:11 -0400

    Initial Commit

---
+++ RumRunner/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RumRunner
+//
+//  Created by Farris Owenati on 7/18/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RumRunner/RumRunnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RumRunnerApp.swift
+//  RumRunner
+//
+//  Created by Farris Owenati on 7/18/23.
+//
+
+import SwiftUI
+
+@main
+struct RumRunnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
